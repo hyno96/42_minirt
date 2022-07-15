8f0091176f83fc43c56403fec127688f84cb5d81
NO HYEONSEOK <nohyeonseok@NOui-MacBookAir.local>
2022-07-15 14:31:07 +0900

    체커보드 y축 정상화

--- srcs/render/checkerboard.c
+++ srcs/render/checkerboard.c
@@ -14,7 +14,7 @@ static t_color3	get_sphere(t_hit_record record)
 	sphere = *conv_sp(record.obj);
 	get_xy_mapping_sphere(&x, &y, record.normal_unit, sphere);
 	divide2 = (int)(2 * M_PI * sphere.radius * x / record.surf.checker.x_range);
-	divide2 += (int)(2 * M_PI * sphere.radius * y / record.surf.checker.y_range);
+	divide2 += (int)(M_PI * sphere.radius * y / record.surf.checker.y_range);
 	if (divide2 % 2 == 0)
 		return (record.surf.checker.color1);
 	return (record.surf.checker.color2);
